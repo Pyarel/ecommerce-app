244681b58a628c3db5f85d4c6b636425ea17d4b9
Pyarel <pyarel.fer@gmail.com>
2025-11-24 14:39:12 +0300

    generate HTML for home and checkout page components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import { HomePage } from "./pages/HomePage";
 import { Route, Routes } from "react-router";
 import "./App.css";
+import axios from "axios";
 import { CheckoutPage } from "./pages/checkout/CheckoutPage";
 import { OrdersPage } from "./pages/OrdersPage";
 import { TrackingPage } from "./pages/TrackingPage";
 import { NotFoundPage } from "./pages/NotFoundPage";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/cart-items?expand=product").then((response) => {
+      setCart(response.data);
+    });
+  }, []);
+
   return (
     <Routes>
-      <Route index element={<HomePage />} />
-      <Route path="checkout" element={<CheckoutPage />} />
+      <Route index element={<HomePage cart={cart} />} />
+      <Route path="checkout" element={<CheckoutPage cart={cart} />} />
       <Route path="orders" element={<OrdersPage />} />
       <Route path="tracking" element={<TrackingPage />} />
       <Route path="*" element={<NotFoundPage />} />
